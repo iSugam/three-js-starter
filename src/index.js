88d8b836fa85743a0b7689eefa26c38c38ba8643
unknown <sugamanandsahu01@gmail.com>
2022-08-16 14:24:20 +0530

    Adding Comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import texture from "./assets/textures/2k_earth_daymap.jpg"
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Add webgl Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -16,25 +17,34 @@ window.addEventListener("resize", () => {
     camera.updateProjectionMatrix()
 });
 
+
 // Add Scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0,0.1, 5) // x, y, and z positions
 
+// Create earth geometry and add the texture material to it and combine it with THREE.Mesh
 const earthGeometry = new THREE.SphereGeometry( 1, 100, 100 );
-const textures = new THREE.TextureLoader().load(texture)
+const textures = new THREE.TextureLoader().load(texture) // Load Texture image
 const material = new THREE.MeshStandardMaterial( {map: textures} );
+
 const earth = new THREE.Mesh( earthGeometry, material );
 scene.add( earth );
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.x = 1.5
-scene.add(directionalLight)
+// Add orbit controls to camera
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
 
+// Add Directional Light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+directionalLight.position.x = 2;
+scene.add(directionalLight);
+
+// Recursive function to keep re-rendering
 function animate() {
     requestAnimationFrame(animate);
-    earth.rotation.y += 0.01
-
+    earth.rotation.y += 0.003;
+    controls.update();
     renderer.render(scene, camera);
 }
 animate()
